992e965fcbd0a039a21a84a535269b82df133e60
Ivanilson Barbosa da Silva <ivanilson.barbosa@academico.ifrn.edu.br>
2019-12-04 09:09:40 -0300

    tabelas adicionadas

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Regiao_corporal(models.Model):
+	nome=models.CharField(max_length=100)
+
+class Tempo_repeticao(models.Model):
+	nome=models.CharField(max_length=100)
+
+class Intervalo_treinos(models.Model):
+	nome=models.CharField(max_length=100)
+
+class Treino(models.Model):
+	nome=models.CharField(max_length=100)
+	dia_semana = models.CharField(max_length=100)
+	regiao=models.ManyToManyField(Regiao_corporal)
+	tempo_repaticao=models.ForeignKey(Tempo_repeticao,on_delete=models.CASCADE,null=True)
+	intervalo_treinos=models.ForeignKey(Intervalo_treinos,on_delete=models.CASCADE,null=True)
+
+"""
+
+dia da semana 
+nome do treino
+região corporal
+tempo/repetição
+intervalo entre os treinos
+
+"""
